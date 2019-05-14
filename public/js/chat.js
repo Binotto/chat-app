b47d61bbf691f0ddbb6405792ed35e9e88e755fa
Matheus <Binotto>
2019-05-13 21:45:27 -0300

    Sharing your location

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,3 +10,16 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     const message = e.target.elements.message.value
     socket.emit('sendMessage', message)
 })
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if(!navigator.geolocation){
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
+})
--- src/index.js
+++ src/index.js
@@ -26,6 +26,10 @@ io.on('connection', (socket) => {
         io.emit('message', message)
     })
 
+    socket.json.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    })
+
     socket.on('disconnect', () => {
         io.emit('message', 'A user has left!')
     })
