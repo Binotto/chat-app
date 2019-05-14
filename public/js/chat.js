c3f4cc62172d7d42fdccebb6e5de9cfb47f953d6
Matheus <Binotto>
2019-05-14 17:10:12 -0300

    Working with time

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,9 +12,9 @@ const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 
 socket.on('message', (message) => {
-    console.log(message)
     const html = Mustache.render(messageTemplate, {
-        message: message 
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const { generateMessage } =require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)  
@@ -20,8 +21,8 @@ let count = 0
 io.on('connection', (socket) => {
     console.log('New WebSockets connection')
 
-    socket.emit('message', 'Welcome User')
-    socket.broadcast.emit('message', 'A new user has joined!')
+    socket.emit('message', generateMessage('Welcome'))
+    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
 
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
@@ -30,7 +31,7 @@ io.on('connection', (socket) => {
             return callback('Shut up mother fucker, Profanity is not allowed!')
         }
 
-        io.emit('message', message)
+        io.emit('message', generateMessage(message))
         callback()
     })
 
@@ -40,7 +41,7 @@ io.on('connection', (socket) => {
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left!')
+        io.emit('message', generateMessage('A user has left!'))
     })
 })
 
